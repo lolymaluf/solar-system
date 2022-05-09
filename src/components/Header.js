a8f09730154e2ec90f0dcf56caa39c3292a05da2
Loly Maluf <lolymaluf@gmail.com>
2022-05-09 18:32:17 -0300

    Adiciona requisito 03 e corrige 01 e 02

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import React from 'react';
 /* Fonte: live-lectures
 https://github.com/tryber/sd-021-a-live-lectures/blob/lecture/10.2/course/src/App.js */
 
-class SolarSystem extends React.Component {
+class Header extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system"> </div>
+      <header><h1>Sistema Solar</h1></header>
     );
   }
 }
 
-export default SolarSystem;
+export default Header;
