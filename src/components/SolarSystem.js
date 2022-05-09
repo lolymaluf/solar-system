b0a718912126a09a48d876a5a8336f46d979ec55
Loly Maluf <lolymaluf@gmail.com>
2022-05-09 17:45:43 -0300

    adiciona requisito 02

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,3 +1,11 @@
 import React from 'react';
 
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testeid="solar-system"> </div>
+    );
+  }
+}
+
 export default SolarSystem;
