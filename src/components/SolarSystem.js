f00042303704dc8a0f0a3a65bcdf2801b1692fd5
Loly Maluf <lolymaluf@gmail.com>
2022-05-09 22:22:51 -0300

    Adiciona requisito 06

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import PlanetCard from './PlanetCard';
 import Title from './Title';
+import planets from '../data/planets';
 
 /* Fonte: live-lectures
 https://github.com/tryber/sd-021-a-live-lectures/blob/lecture/10.2/course/src/App.js */
@@ -9,6 +11,13 @@ class SolarSystem extends React.Component {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map((callback) => (
+          <PlanetCard
+            key={ callback.name }
+            planetImage={ callback.image }
+            planetName={ callback.name }
+          />
+        ))}
       </div>
     );
   }
