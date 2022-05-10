9e5e7baa1ee371e57e9eee65686f3aa741c0ee4e
Loly Maluf <lolymaluf@gmail.com>
2022-05-09 22:30:11 -0300

    Adiciona Requisito 07

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Title from './components/Title';
+import Missions from './components/Missions';
 /* import PlanetCard from './components/PlanetCard'; */
 
 class App extends React.Component {
@@ -11,6 +12,7 @@ class App extends React.Component {
         <Header />
         <SolarSystem />
         <Title />
+        <Missions />
       </div>
     );
   }
